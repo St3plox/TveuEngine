3fb484ca292259e68bbb274d60e278d5eccbe233
st3pegor <st3pegor@gmail.com>
2023-11-03 00:01:04 +0700

    added window starting logic

--- src/main/java/com/tveu/Main.java
+++ src/main/java/com/tveu/Main.java
@@ -1,7 +1,10 @@
 package com.tveu;
 
+import com.tveu.core.Window;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Window window = Window.get();
+        window.run();
     }
 }
